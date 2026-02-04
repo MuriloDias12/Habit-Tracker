d1a5ee48a026e599473283f645fc2ea4cc104f2c
MuriloDias12 <murilodiasdeoliveira>
2026-02-04 16:38:47 -0300

    FEAT : Criando front end e instalando node

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Habit Tracker</h1>
+
+      <button>Criar novo hábito</button>
+
+      <ul>
+        <li>Beber água</li>
+        <li>Estudar Java</li>
+        <li>Treinar</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
